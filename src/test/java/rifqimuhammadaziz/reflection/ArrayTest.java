90162a4e1da5a4e1e88ef52291ea5c1c3a500976
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-21 16:06:43 +0700

    create test Reflection Array

---
+++ src/test/java/rifqimuhammadaziz/reflection/ArrayTest.java
@@ -0,0 +1,50 @@
+package rifqimuhammadaziz.reflection;
+
+import org.junit.jupiter.api.Test;
+import rifqimuhammadaziz.reflection.data.Person;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayTest {
+
+    @Test
+    void testArrayClass() {
+        Class<String[]> stringArrayClass = String[].class;
+        Class<int[]> intArrayClass = int[].class;
+        Class<Person> personClass = Person.class;
+
+        System.out.println(stringArrayClass.isArray());
+        System.out.println(intArrayClass.isArray());
+        System.out.println(personClass.isArray());
+    }
+
+    @Test
+    void testArrayMember() {
+        Class<String[]> stringArrayClass = String[].class;
+
+        // array have no fields, constructors, methods
+        System.out.println(Arrays.toString(stringArrayClass.getDeclaredFields()));
+        System.out.println(Arrays.toString(stringArrayClass.getDeclaredConstructors()));
+        System.out.println(Arrays.toString(stringArrayClass.getDeclaredMethods()));
+        System.out.println(stringArrayClass.getComponentType()); // string
+    }
+
+    @Test
+    void testArrayManipulation() {
+        Class<String[]> stringArrayClass = String[].class;
+        String[] array = (String[]) Array.newInstance(stringArrayClass.getComponentType(), 10);
+
+        System.out.println(Arrays.toString(array));
+
+        Array.set(array, 0, "Rifqi");
+        Array.set(array, 1, "Muhammad");
+        Array.set(array, 2, "Aziz");
+
+        System.out.println(Arrays.toString(array));
+
+        System.out.println(Array.get(array, 0));
+        System.out.println(Array.get(array, 1));
+        System.out.println(Array.get(array, 2));
+    }
+}
